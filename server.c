735458dcd68e466ae3caaf369562486dc3d0f9da
jeg99 <a00824494@itesm.com>
2021-06-14 20:06:39 -0500

    agregar estructura para secuencias y base para paralelizar mapeo

--- server.c
+++ server.c
@@ -9,9 +9,18 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+// Multithreading
+#include <pthread.h>
+
 #define MAX_LEN 1000000
 
 char *genome;
+
+struct SequencesStruct {
+  int pool_size;
+  int size;
+  char **seqs;
+} sequences;
 
 char *strremove(char *str, const char *sub) {
   char *p, *q, *r;
@@ -61,11 +70,42 @@ void read_genome(int client_socket) {
   return;
 }
 
+char **append(char **oldMatrix, int *size, const char str[MAX_LEN]) {
+  for(int i = 0; i < *size; i++){
+    if(strcmp(oldMatrix[i], str) == 0){
+      return oldMatrix;
+    }
+  }
+
+  char **newMatrix = (char **)realloc(oldMatrix, (*size + 1)*sizeof(char *));
+  newMatrix[*size] = (char *)malloc(MAX_LEN*sizeof(char));
+  strcpy(newMatrix[*size], str);
+  (*size)++;
+
+  return newMatrix;
+}
+
+void *map(void* arg) {
+  int *index = (int *)arg;
+  char *buff;
+  buff = malloc(MAX_LEN);
+  buff = sequences.seqs[*index];
+
+  printf("Test #%d = ", *index);
+
+  if (strstr(genome, buff) == NULL) {
+    printf("Not found\n");
+  } else {
+    printf("Found \n");
+  }
+
+}
+
 void search_sequences(int client_socket) {
   char *buff;
   buff = malloc(MAX_LEN);
 
-  int number_of_test = 1;
+  int number_of_test = 0;
   int end_flag = 0;
 
   for (;;) {
@@ -78,6 +118,8 @@ void search_sequences(int client_socket) {
       end_flag = 1;
     }
 
+    sequences.seqs = append(sequences.seqs, &sequences.size, buff);
+/*
     printf("Test #%d = ", number_of_test);
 
     if (strstr(genome, buff) == NULL) {
@@ -86,12 +128,29 @@ void search_sequences(int client_socket) {
       printf("Found \n");
     }
     number_of_test++;
-
+*/
     if(end_flag) break;
   }
+
+  int rc;
+  pthread_t threads[sequences.pool_size];
+  for (int i = 0; i < sequences.size; i+=sequences.pool_size) {
+
+    for(int j = 0; j < sequences.pool_size; j++) {
+      rc = pthread_create(&threads[i], NULL, map, (void *)&i);
+    }
+    //number_of_test++;
+
+    //if(end_flag) break;
+  }
+
 }
 
 int main () {
+
+  sequences.size = 0;
+  sequences.pool_size = 8;
+
   // create socket
   int server_socket;
   server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -133,6 +192,10 @@ int main () {
       
     }
   } while (read_size > 0);
+
+  for(int i = 0; i < sequences.size; i++) {
+    printf("%s\n", sequences.seqs[i]);
+  }
 
   if (read_size == 0) {
     puts("Client disconnected");
